7520ea7361a87f4134865e837be286347f33ff4a
Lunathor <mrcoopgmz@gmail.com>
2025-01-11 23:36:27 +0300

    Добавлены основные представления и комментарии к коду для лучшего понимания преподавателя и в целом при разборе кода в будущем

--- messenger/chatik/views.py
+++ messenger/chatik/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from models import *
 from .serializers import *
 from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 import logging
+from django.http import JsonResponse, Http404
+from django.views import View
 # Create your views here.
 logger = logging.getLogger(__name__)
 
@@ -60,3 +62,54 @@ class UserView(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         # Возвращаем ошибки валидации, если данные не валидны
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChatViewSet(viewsets.ModelViewSet):
+    # Определяем набор данных, который будет использоваться в этом представлении
+    queryset = Chat.objects.all()
+    
+    # Указываем сериализатор, который будет использоваться для преобразования данных
+    serializer_class = ChatSerializer
+    
+    # Указываем классы разрешений, которые определяют доступ к этому представлению
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def perform_create(self, serializer):
+        # Метод, который вызывается при создании нового объекта
+        # Здесь можно добавить дополнительную логику перед сохранением (если понадобиться в будущем при масштабировании)
+        serializer.save()
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    # Определяем набор данных для сообщений
+    queryset = Message.objects.all()
+    
+    # Указываем сериализатор, который будет использоваться для преобразования данных
+    serializer_class = MessageSerializer
+    
+    # Указываем классы разрешений, которые определяют доступ к этому представлению
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def perform_create(self, serializer):
+        # Устанавливаем текущего пользователя как отправителя сообщения
+        serializer.save(user=self.request.user)
+
+
+class ChatMessagesView(View):
+    def get(self, request, chat_id):
+        try:
+            # Проверяем существование чата по его идентификатору
+            current_chat = get_object_or_404(Chat, id=chat_id)
+            
+            # Получаем все сообщения, связанные с этим чатом
+            messages = Message.objects.filter(chat=current_chat)
+            
+            # Сериализуем сообщения для отправки в ответе
+            serializer = MessageSerializer(messages, many=True)
+            
+            # Возвращаем сериализованные данные в формате JSON
+            return JsonResponse(serializer.data, safe=False)
+        
+        except Http404:
+            # Обрабатываем ошибку 404, если чат не найден
+            return JsonResponse({'error': 'Chat not found'}, status=404)
