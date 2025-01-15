1c056a5284c7b4e56597bf84a8b5efb0a4e25348
Lunathor <mrcoopgmz@gmail.com>
2025-01-15 15:50:02 +0300

    Стыдно признавать но все ошибки были в том что я потерял в основном файле urls добавления urls из приложения

--- messenger/messenger/urls.py
+++ messenger/messenger/urls.py
@@ -19,10 +19,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib import admin
+from chatik.urls import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('chatik.urls')),  # Подключаем маршруты из приложения chat
+    path('', include('chatik.urls')),  # Подключаем маршруты из приложения chatik
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
